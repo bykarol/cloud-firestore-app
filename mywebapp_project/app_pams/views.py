48cef640421fd2cd24b36565f34b41939da50a7c
bykarol <brachokarol.sud@gmail.com>
2024-06-22 22:53:43 +0200

    update patient completed, update readme

--- mywebapp_project/app_pams/views.py
+++ mywebapp_project/app_pams/views.py
@@ -24,6 +24,9 @@ def homepage(request):
        patient_dict = patient.to_dict()
        # Adding the id to the each patient
        patient_dict['id'] = patient.id
+      # Format date_of_birth to YYYY-MM-DD
+       if 'date_of_birth' in patient_dict and isinstance(patient_dict['date_of_birth'], datetime.date):
+           patient_dict['date_of_birth'] = patient_dict['date_of_birth'].strftime('%d-%m-%Y')
        patients_list.append(patient_dict)
 
    return render(request, "homepage.html", {'patients': patients_list})
@@ -54,13 +57,57 @@ def newpatient(request):
         form = PatientForm()
     return render(request, "patientform.html", {'form': form})
 
+# Update a patient
 def updatepatient(request, patient_id):
-    return render(request, "updatepatient.html")
+    try:
+        # Getting the patient
+        patient_ref = db.collection('patients').document(patient_id)
+        patient = patient_ref.get()
+
+        # Verifying if patient exists in Firestore
+        if patient.exists:
+            # Extract the data from the document
+            patient_data = patient.to_dict()
+
+            # parsing date
+            if 'date_of_birth' in patient_data:
+                patient_data['date_of_birth'] = patient_data['date_of_birth'].strftime('%Y-%m-%d')
+
+            # Setting the form with the current patient data
+            form = PatientForm(initial=patient_data)
+
+            # Sent form updated
+            if request.method == 'POST':
+                form = PatientForm(request.POST)
+
+                if form.is_valid():
+                    # Updating the patient with the new data
+                    newpatientdata = form.cleaned_data
+                    if 'date_of_birth' in newpatientdata and isinstance(newpatientdata['date_of_birth'], datetime.date):
+                        newpatientdata['date_of_birth'] = datetime.datetime.combine(newpatientdata['date_of_birth'], datetime.datetime.min.time())
+                    patient_ref.update(newpatientdata)
+                    messages.success(request, f'Patient with ID {patient_id} updated successfully!')
+                    return redirect('homepage')
+
+            # Render form with user data
+            context = {
+                'patient': patient_data,
+            }
+            return render(request, 'updatepatient.html', context)
+        else:
+            # Patient doesn't exist
+            messages.error(request, f'Patient with ID {patient_id} does not exist.')
+            return redirect('homepage')
+
+    except Exception as e:
+        # Any exception redirect to homepage
+        messages.error(request, f'Error updating patient details: {e}')
+        return redirect('homepage')
 
 # Delete a patient
 def deletepatient(request, patient_id):
     try:
-        # Eliminar el documento usando el ID proporcionado por Firestore
+        # Deleting the document by id
         db.collection('patients').document(patient_id).delete()
         messages.success(request, f'Patient with ID {patient_id} deleted successfully!')
     except Exception as e:
